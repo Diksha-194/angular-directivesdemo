48032c63c3a5120258b5ba93967e7522ffc362d1
Diksha-194 <dikshamankar24@gmail.com>
2019-12-19 11:33:48 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,23 +15,80 @@ export class AppComponent  {
   show() {
     this.showParagraph = true;
   }
+blueColor: boolean = false;  
+   blue() {
+    this.blueColor = true;    
+  }
 
 
-  carnames: object[] = [
+  users: object[] = [
     {
-      cars: 'Audi'
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz', 
+      address: {
+        street: 'Victor Plains',
+        suite: 'Suite 879',
+        city: 'Wisokyburgh',
+        zipcode: '90566-7771'
+      },     
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org'
     },
     {
-      cars: 'BMW'
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      address: {
+        street: 'Victor Plains',
+        suite: 'Suite 879',
+        city: 'Wisokyburgh',
+        zipcode: '90566-7771'
+      },  
+      phone: '010-692-6593 x09125',
+      website: 'anastasia.net'
     },
     {
-      cars: 'Aston Martin'
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',  
+      address: {
+        street: 'Douglas Extension',
+        suite: 'Suite 847',
+        city: 'McKenziehaven',
+        zipcode: '59590-4157'
+      },     
+      phone: '1-463-123-4447',
+      website: 'ramiro.info'
+    },
+    {
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      address: {
+        street: 'Hoeger Mall',
+        suite: 'Apt. 692',
+        city: 'South Elvis',
+        zipcode: '53919-4257'
+      },      
+      phone: '493-170-9623 x156',
+      website: 'kale.biz'
+    },    
+    {
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',  
+      address: {
+        street: 'Skiles Walks',
+        suite: 'Suite 351',
+        city: 'Roscoeview',
+        zipcode: '33263'
+      },    
+      phone: '(254)954-1289',
+      website: 'demarco.info',
     }
+       
   ];
 
-  blueColor: boolean = false;  
-   blue() {
-    this.blueColor = true;
-    
-  }
+  
 }
